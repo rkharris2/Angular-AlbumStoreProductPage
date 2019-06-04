41355cb67d446b902042ff0f99807ad01b673213
Ryan Harris <Ryan.Harris@altec.com>
2019-06-04 13:23:58 -0500

    Added Album + Track interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,7 +4,8 @@ import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
 
-
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -13,7 +14,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number): Observable<Album>{
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
